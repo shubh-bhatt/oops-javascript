68e5a5d37267e9c5024aa7c54c430e847a9aca90
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-06-28 16:41:27 +0530

    this and new keyword

--- 01/script.js
+++ 01/script.js
@@ -1,7 +1,9 @@
-function House() {
-  this.location = "Delhi",
-  this.rooms = 3,
-  this.kitchen = 1
+function House(location, rooms, kitchen) {
+  this.location = location,
+  this.rooms = rooms,
+  this.kitchen = kitchen
 }
 
-let myHouse = new House();
+let myHouse1 = new House("Delhi", 3, 1);
+let myHouse2 = new House("Noida", 4, 2);
+let myHouse3 = new House("Dehradun", 5, 2);
