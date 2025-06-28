9ce8c960ab23be21662b851c7c2e14f84f74cc9e
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-06-28 19:50:48 +0530

    prototype in js

--- 03-prototype/script.js
+++ 03-prototype/script.js
@@ -8,11 +8,19 @@ class Toffee {
 }
 
 Toffee.prototype.price = 10; //price common to all using prototype
+Toffee.prototype.isHealthy = function () {
+  console.log("Healthy to eat");
+};
 
 let t1 = new Toffee("Strawberry", "Pink", "Sweet", "Round");
 let t2 = new Toffee("Chocolate", "Brown", "Sweet", "Square");
 let t3 = new Toffee("Vanilla", "White", "Sweet", "Heart");
 
 console.log(t1.price);
+console.log(t1.isHealthy());
+
 console.log(t2.price);
+console.log(t2.isHealthy());
+
 console.log(t3.price);
+console.log(t3.isHealthy());
