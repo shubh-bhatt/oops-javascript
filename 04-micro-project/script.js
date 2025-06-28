a169f1d26f33999c4cc27265fe3da78fecca2081
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-06-28 23:09:13 +0530

    Library micro project

---
+++ 04-micro-project/script.js
@@ -0,0 +1,52 @@
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(book) {
+    //regular method
+    this.books.push(book);
+  }
+
+  listAllBooks() {
+    this.books.forEach((book, index) => {
+      console.log(`${index + 1}) ${book.name} by ${book.author}`);
+    });
+  }
+}
+
+class Book {
+  constructor(name, isbn, price, author) {
+    this.name = name;
+    this.isbn = isbn;
+    this.price = price;
+    this.author = author;
+    this.readStatus = false;
+  }
+
+  info() {
+    console.log(
+      `${this.name} is written by ${this.author} and you have ${
+        this.readStatus ? "read it" : "not read this book"
+      } and is available at amazon at the price range of ${this.price}.`
+    );
+  }
+
+  changeReadStatus() {
+    this.readStatus = !this.readStatus;
+  }
+}
+
+let book1 = new Book(
+  "The Intelligent Investor",
+  1654651,
+  420,
+  "Benjamin Graham"
+);
+let book2 = new Book("Snowball", 12124241, 310, "Mary Buffett");
+let book3 = new Book("Beating the Street", 875551, 350, "Peter Lynch");
+
+let delhiLibrary = new Library();
+delhiLibrary.addBook(book1);
+delhiLibrary.addBook(book2);
+delhiLibrary.addBook(book3);
